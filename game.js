22c2f5cc9ace9f2261123b72a701860d6b4cab3f
EvaTP <evathpr@icloud.com>
2025-02-18 17:15:58 +0100

    ajout fonctions affichage options

--- game.js
+++ game.js
@@ -11,26 +11,50 @@ newParagraph.innerText = quiz_espace.intro
 const quizEspaceElement = document.querySelector('#question-text')
 console.log("première question")
 
+// options
+const choixOptions = document.querySelector('#options-container')
+// boutons options
+const boutonOption = document.createElement('button')
+
+
 quizEspaceElement.appendChild(newParagraph)
 
 // bouton "Let's go!"
 const boutonStart = document.querySelector('#start-button')
+// bouton "Suivant"
 const boutonSuivant = document.querySelector('#next-button')
+
+
 console.log(boutonSuivant)
+
+let textIndex = 0;
+
+// affichage de la première question & de ses options
 boutonStart.addEventListener("click", function(){
-    for (const item of quiz_espace.questions) {
-        console.log('😶‍🌫️', item.questions)
-        boutonStart.classList.add('hidden')
-        boutonSuivant.classList.remove('hidden')  // faire apparaitre le bouton "suivant"
+    console.log("premiere question")
+    quizEspaceElement.innerText = quiz_espace.questions[textIndex].text
+
+    choixOptions.innerText = quiz_espace.questions[textIndex].options   
+   
+    boutonStart.classList.add('hidden')
+
+    boutonSuivant.classList.remove('hidden')  // faire apparaitre le bouton "suivant"
+
+
+    // for (const item of quiz_espace.questions) {
+    //     console.log('😶‍🌫️', questions[textIndex].text)
+    //    //console.log('😶‍🌫️', item.questions)
+    //     boutonStart.classList.add('hidden')
+    //     boutonSuivant.classList.remove('hidden')  // faire apparaitre le bouton "suivant"
 
     
-        const newButton = document.createElement('button')
-        newButton.innerText = item.text
-        newButton.style.color = 'blue'
-        newButton.style.fontSize = '20px'
+    //     const newButton = document.createElement('button')
+    //     newButton.innerText = item.text
+    //     newButton.style.color = 'blue'
+    //     newButton.style.fontSize = '20px'
     
-        quizEspaceElement.appendChild(newButton)
-    }
+    //     quizEspaceElement.appendChild(newButton)
+    // }
 })
 
 
