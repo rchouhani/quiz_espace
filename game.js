7a946cf056ab5dc8bd5adaddd5f23a122a99c452
EvaTP <evathpr@icloud.com>
2025-02-18 13:56:01 +0100

    ajout fonctions sur boutons et intro

--- game.js
+++ game.js
@@ -1,16 +1,50 @@
 import { quiz_espace } from './questions.js'; // Import des questions
 
+
+// Paragraphe intro
+const newParagraph = document.querySelector('#intro')
+console.log("paragraphe intro")
+newParagraph.innerText = quiz_espace.intro
+
+
+// questions
 const quizEspaceElement = document.querySelector('#question-text')
 console.log("première question")
 
-for (const item of quiz_espace.questions) {
-    console.log('😶‍🌫️', item.questions)
+quizEspaceElement.appendChild(newParagraph)
 
-    const newButton = document.createElement('button')
-    newButton.innerText = item.text
-    newButton.style.color = 'blue'
-    newButton.style.fontSize = '20px'
+// bouton "Let's go!"
+const boutonStart = document.querySelector('#start-button')
+const boutonSuivant = document.querySelector('#next-button')
+console.log(boutonSuivant)
+boutonStart.addEventListener("click", function(){
+    for (const item of quiz_espace.questions) {
+        console.log('😶‍🌫️', item.questions)
+        boutonStart.classList.add('hidden')
+        boutonSuivant.classList.remove('hidden')  // faire apparaitre le bouton "suivant"
+
+    
+        const newButton = document.createElement('button')
+        newButton.innerText = item.text
+        newButton.style.color = 'blue'
+        newButton.style.fontSize = '20px'
+    
+        quizEspaceElement.appendChild(newButton)
+    }
+})
 
 
-    quizEspaceElement.appendChild(newButton)
-}
+
+
+
+
+
+
+
+
+
+
+// for (const item of quiz_espace.questions){
+//     console.log("hello", item.intro)
+// }
+
